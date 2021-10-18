2fbe03649e3ab3ebc86ebb6fb58a744056a00933
anthony <Muthumatonito681@gmail.com>
2021-10-18 23:24:15 +0300

    1.updated landing_page.dart, main.dart, nav_bar.dart 2. Added about_page.dart, contactUs_page.dart, home_page.dart

--- lib/about_page/about_page.dart
+++ lib/about_page/about_page.dart
@@ -13,14 +13,15 @@ class _AboutPageState extends State<AboutPage> {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
+      decoration: BoxDecoration(color: Colors.white
+          /*gradient: LinearGradient(
               begin: Alignment.centerLeft,
               end: Alignment.centerRight,
               colors: [
             Color.fromRGBO(12, 6, 45, 1.0),
             Color.fromRGBO(23, 8, 33, 1.0)
-          ])),
+          ])*/
+          ),
       child: Column(
         children: [
           Padding(
@@ -33,6 +34,20 @@ class _AboutPageState extends State<AboutPage> {
                   fontSize: 20),
             ),
           ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.8,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.grey
+                /*gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Color.fromRGBO(116, 92, 227, 1.0),
+                      Color.fromRGBO(195, 108, 245, 1.0)
+                    ])*/
+                ),
+          )
         ],
       ),
     );
